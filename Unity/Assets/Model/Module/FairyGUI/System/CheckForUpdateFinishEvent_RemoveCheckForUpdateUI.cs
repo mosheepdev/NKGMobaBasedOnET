adc024c0478e93b2a42e957f5685bd1612068204
NKGMadlife <1778139321@qq.com>
2019-04-28 20:25:34 +0800

    重构资源更新部分逻辑

---
+++ Unity/Assets/Model/Module/FairyGUI/System/CheckForUpdateFinishEvent_RemoveCheckForUpdateUI.cs
@@ -0,0 +1,12 @@
+﻿namespace ETModel
+{
+    [Event(EventIdType.CheckForUpdateFinish)]
+    public class CheckForUpdateFinishEvent_RemoveCheckForUpdateUI : AEvent
+    {
+        public override void Run()
+        {
+			Game.Scene.GetComponent<FUIComponent>().Remove("CheckForResUpdate");
+			Log.Info("检查更新界面关闭");
+        }
+    }
+}
